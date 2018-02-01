8aeb5e7c23b06f04637505cf62e5da53254ce51c
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-01 16:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-//+ Create a function `theBeatlesPlay`, which accepts two parameters- an array of musicians and an array of instruments. 
+//+ Create a function `theBeatlesPlay`, which accepts two parameters- an array of musicians and an array of instruments.
 
 function theBeatlesPlay(musicians, instruments){
-  //The body of the function should create an empty array stored in a variable. 
+  //The body of the function should create an empty array stored in a variable.
   var arr = [];
   //The function should also contain a for loop which loops over the array of musicians. You'll want to be careful about what value you set your counter variable to store. (Hint: Think about what the first index of an array is).
       for (var i = 0; i < musicians.length; i++){
-      //The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: `"John Lennon plays guitar"`. 
+      //The first time through the loop, the body of the loop should create a string using the first index of the musicians array and the first index of the instruments array: `"John Lennon plays guitar"`.
       //This string should be added to the empty array you created. The loop should make the same sentence for every member of the musicians array.
-      arr = musicians[i] + " plays " + instruments[i]; 
+      arr = musicians[i] + " plays " + instruments[i];
       }
   //The function should return the array of new strings.
   return arr;
